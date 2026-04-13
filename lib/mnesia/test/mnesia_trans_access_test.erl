a8ca4dbc2ce720666f240c7d4c2209d7f527643b
Michał Wąsowski <michal@erlang.org>
2026-04-13 11:41:54 +0200

    Return stacktrace when transaction aborts due to error

--- lib/mnesia/test/mnesia_trans_access_test.erl
+++ lib/mnesia/test/mnesia_trans_access_test.erl
@@ -42,7 +42,7 @@
          create_live_table_index_disc_only/1, del_table_index_ram/1,
          del_table_index_disc/1, del_table_index_disc_only/1,
          idx_schema_changes_ram/1, idx_schema_changes_disc/1,
-         idx_schema_changes_disc_only/1]).
+         idx_schema_changes_disc_only/1, stacktrace_return/1]).
 
 -export([do_nested/1]).
 
@@ -69,7 +69,7 @@ all() ->
     [write, read, wread, delete, delete_object_bag, delete_object_set,
      match_object, select, select14, all_keys, transaction, transaction_counters,
      {group, nested_activities}, {group, index_tabs},
-     {group, index_lifecycle}].
+     {group, index_lifecycle}, stacktrace_return].
 
 groups() ->
     [{nested_activities, [],
@@ -1432,3 +1432,27 @@ idx_schema_changes(Config, Storage) ->
     ?match([{Tab, 16, 66}], rpc:call(N1, mnesia, dirty_index_read, [Tab, 66, Idx])),
 
     ?verify_mnesia(Nodes, []).
+
+%% Check return value of aborted mnesia:transaction/1
+
+stacktrace_return(suite) -> [];
+stacktrace_return(Config) ->
+    [_N] = ?acquire_nodes(1, Config),
+    Throw = fun() -> throw(test) end,
+    Exit = fun() -> exit(test) end,
+    Error = fun() -> error(test) end,
+
+    ?match({aborted, {throw, test}}, mnesia:transaction(Throw)),
+    ?match({aborted, test}, mnesia:transaction(Exit)),
+    ?match({aborted, {test, [{?MODULE, _, _, _} | _]}}, mnesia:transaction(Error)),
+
+    Nested = fun(F) ->
+        fun() ->
+            {aborted, Reason} = mnesia:transaction(F),
+            mnesia:abort(Reason)
+        end
+    end,
+
+    ?match({aborted, {throw, test}}, mnesia:transaction(Nested(Throw))),
+    ?match({aborted, test}, mnesia:transaction(Nested(Exit))),
+    ?match({aborted, {test, [{?MODULE, _, _, _} | _]}}, mnesia:transaction(Nested(Error))).
